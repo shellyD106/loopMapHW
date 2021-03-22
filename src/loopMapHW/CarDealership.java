e9a9e3059887ba2673f0c409f1c532a1cd6be57a
Shelda <sdelassin@gmail.com>
2021-03-22 14:58:00 -0400

    project commit

--- src/loopMapHW/CarDealership.java
+++ src/loopMapHW/CarDealership.java
@@ -1,5 +1,42 @@
 package loopMapHW;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class CarDealership {
+	public static void main(String[] args) {
+		HashMap<String, ArrayList<Integer>> vehicles = new HashMap<String, ArrayList<Integer>>();
+
+        
+        vehicles.put("Rav4", new ArrayList<Integer>(Arrays.asList(2003, 2010, 2017)));
+        vehicles.put("Pruis", new ArrayList<Integer>(Arrays.asList(2008, 2020)));
+        vehicles.put("Avalon", new ArrayList<Integer>(Collections.singletonList(2017)));
+        vehicles.put("Camry", new ArrayList<Integer>(Arrays.asList(2015, 2016, 2017)));
+        vehicles.put("Corolla", new ArrayList<Integer>(Arrays.asList(2015, 2017, 2020)));
+
+       
+        Scanner sc = new Scanner(System.in);
+        
+       
+        String input = sc.nextLine();
+
+        
+        if(vehicles.containsKey(input)) {
+            System.out.println("Oh, you're looking for a" + input + "?");
+            
+             
+            ArrayList<Integer> list = vehicles.get(input);
+
+            for (Integer make : list) {
+                System.out.println("Name: " + input + " make: " + make);
+            }
+        }else {
+            
+        	System.out.println("Sorry we currently do not have any " + input + " vehicle");
+        }
+  }
 
 }
--- src/loopMapHW/LoopMap.java
+++ src/loopMapHW/LoopMap.java
@@ -1,5 +1,40 @@
 package loopMapHW;
 
-public class LoopMap {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class LoopMap {
+	 public static void main(String[] args) {
+	      
+	        ArrayList<Double> list = new ArrayList<>();
+
+
+	        Scanner sc = new Scanner(System.in);
+
+	        for(int i = 0; i < 5; i++) {
+	            System.out.println("Please enter number " + (i + 1));
+	            list.add(sc.nextDouble());
+	        }
+
+	        
+	        double sum = 0, product = 1, largest = list.get(0), smallest = list.get(0);
+
+	       
+	        for(int i = 0; i < 5; i++) {
+	            sum += list.get(i); 
+	            product *= list.get(i); 
+
+	            largest = Math.max(largest, list.get(i)); 
+	            smallest = Math.min(smallest, list.get(i)); 
+	        }
+
+	        
+	        System.out.println("The sum is: " + sum);
+	        System.out.println("The product is: " + product);
+	        System.out.println("The largest is: " + largest);
+	        System.out.println("The smallest is: " + smallest);
+	    }
 }
+
+
+
